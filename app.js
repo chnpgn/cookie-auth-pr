77f79da152fbe058a71b81b06d0b4d5280800adb
taman raas <t4m4nr44s@gmail.com>
2026-07-03 09:33:48 +0300

    Support multiple roles

--- app.js
+++ app.js
@@ -7,66 +7,66 @@ app.use(express.json());
 app.use(cookieParser());
 
 const users = [
-  { id: 1, username: "admin", role: "admin" },
-  { id: 2, username: "john", role: "user" },
+    { id: 1, username: "admin", role: "admin" },
+    { id: 2, username: "john", role: "user" },
 ];
 
 app.post("/login", (req, res) => {
-  const { username } = req.body;
+    const { username } = req.body;
 
-  const user = users.find((u) => u.username === username);
+    const user = users.find((u) => u.username === username);
 
-  if (!user) {
-    return res.status(401).json({
-      message: "Invalid user",
+    if (!user) {
+        return res.status(401).json({
+            message: "Invalid user",
+        });
+    }
+
+    res.cookie("user", JSON.stringify(user), {
+        httpOnly: true,
     });
-  }
 
-  res.cookie("user", JSON.stringify(user), {
-    httpOnly: true,
-  });
-
-  res.json({
-    message: "Logged in",
-  });
+    res.json({
+        message: "Logged in",
+    });
 });
 
 function authenticate(req, res, next) {
-  const cookie = req.cookies.user;
+    const cookie = req.cookies.user;
 
-  if (!cookie) {
-    return res.status(401).json({
-      message: "Not authenticated",
-    });
-  }
-
-  req.user = JSON.parse(cookie);
-
-  next();
-}
-
-function authorize(role) {
-  return (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).json({
-        message: "Forbidden",
-      });
+    if (!cookie) {
+        return res.status(401).json({
+            message: "Not authenticated",
+        });
     }
 
+    req.user = JSON.parse(cookie);
+
     next();
-  };
 }
 
-app.get("/admin", authenticate, authorize("admin"), (req, res) => {
-  res.json({
-    message: "Admin area",
-  });
+function authorize(...roles) {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: "Forbidden"
+            });
+        }
+
+        next();
+    };
+}
+
+app.get("/admin", authenticate, authorize("admin", "user", "manager"), (req, res) => {
+    res.json({
+        message: "Admin area",
+    });
 });
 
 app.get("/profile", authenticate, (req, res) => {
-  res.json(req.user);
+    res.json(req.user);
 });
 
 app.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+    console.log("Server is running at http://localhost:3000");
 });
